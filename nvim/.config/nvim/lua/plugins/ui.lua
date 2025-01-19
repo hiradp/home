a7624582c1c38b5b32e696644d71bfd3685c98b2
Hirad Pourtahmasbi <git@hirad.codes>
2025-01-18 20:52:33 -0500

    nvim: remove neoconf

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -9,4 +9,27 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".DS_Store",
+            ".git",
+            ".idea",
+            "target",
+          },
+        },
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+      window = {
+        position = "right",
+      },
+    },
+  },
 }
