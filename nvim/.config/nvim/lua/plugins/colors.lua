ab4652867409f9c74714ac52da5f4bd0b8db8ccc
Hirad Pourtahmasbi <git@hirad.codes>
2025-01-21 19:50:05 -0500

    Revert "nvim: remove existing config"
    This reverts commit a5316152e1451e6a6e067bf5a42a1f62a6a589ca.

---
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,17 @@
+return {
+    -- Colors for my eyes
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('github-theme').setup {
+                options = {
+                    transparent = true,
+                },
+            }
+
+            vim.cmd 'colorscheme github_dark_default'
+        end,
+    },
+}
---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,39 @@
+return {
+    -- Search and fzf
+    {
+        'nvim-telescope/telescope.nvim',
+        event = 'VimEnter',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+            },
+            { 'nvim-telescope/telescope-ui-select.nvim' },
+        },
+        config = function()
+            require('telescope').setup {
+                extensions = {
+                    ['ui-select'] = {
+                        require('telescope.themes').get_dropdown(),
+                    },
+                },
+            }
+
+            require('telescope').load_extension 'fzf'
+            require('telescope').load_extension 'ui-select'
+
+            local builtin = require 'telescope.builtin'
+            vim.keymap.set('n', '<leader><leader>', function()
+                local ok = pcall(builtin.git_files)
+                if not ok then
+                    builtin.find_files()
+                end
+            end, { desc = '[ ] Search files' })
+            vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch [T]elescope' })
+        end,
+    },
+}
