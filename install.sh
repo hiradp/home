02bcf222a1b14dde297bd8fcb3ec0d079c5da843
Hirad Pourtahmasbi <git@hirad.codes>
2025-11-20 11:00:53 -0500

    feat: add aerospace

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z $STOW_FOLDERS ]]; then
-  STOW_FOLDERS="fish ghostty git nvim starship tmux"
+  STOW_FOLDERS="aerospace fish ghostty git nvim starship tmux"
 fi
 
 if [[ -z $DOTFILES ]]; then
