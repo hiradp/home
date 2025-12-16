55dcdaff765feba3c75b1557db1b060d58e11d70
Hirad Pourtahmasbi <git@hirad.codes>
2025-12-16 11:20:30 -0500

    feat(fish[cl]): add support for crate-specific scopes

--- fish/.config/fish/functions/c/cl.fish
+++ fish/.config/fish/functions/c/cl.fish
@@ -1,3 +1,7 @@
-function cl
-    c clippy --workspace --tests --benches --examples --bins -- -D warnings
+function cl --argument-names scope
+    if test -n "$scope"
+        c clippy -p $scope --tests --benches --examples --bins -- -D warnings
+    else
+        c clippy --workspace --tests --benches --examples --bins -- -D warnings
+    end
 end
