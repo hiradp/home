3378d39eb0f2418b8ea23c92bbf6b17f91970b68
Hirad Pourtahmasbi <git@hirad.codes>
2026-01-24 10:40:43 -0500

    style(terminal): update appearance with transparency

--- config/nvim/.config/nvim/lua/plugins/ui.lua
+++ config/nvim/.config/nvim/lua/plugins/ui.lua
@@ -13,8 +13,10 @@ return {
     config = function()
       require("gruvbox").setup({
         contrast = "hard",
+        transparent_mode = true,
       })
-      vim.cmd([[colorscheme gruvbox]])
+
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 
